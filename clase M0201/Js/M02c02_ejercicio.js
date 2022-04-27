fc3ee8e48a9f4f36715bf8e1cb5b98c9ebde94f7
katy <magdalena518xdgoldita@gmail.com>
2022-04-27 16:16:45 -0300

    clase 2 modulo 2

---
+++ clase M0201/Js/M02c02_ejercicio.js
@@ -0,0 +1,44 @@
+// let x = 10;
+// let y = "a";
+// console.log ("el valor de y===b es igual a:");
+// console.log  (y === "b" || x >= 10);
+
+///.....................................................
+
+
+// let x = 3;
+// let y = 8;
+
+// console.log("el valor de x==3 es igual a");
+// console.log(x=="3");
+// console.log("el valor de x===y es igual a ");
+// console.log(x===y);
+// console.log("el valor de y !==8 es igual a");
+// console.log(y !==8)
+// console.log("el valor de x<=y es igual a");
+// console.log(x<=y)
+// console.log ("el valor de la operacion total (x == 3 || x === y) && !(y !== 8 && x <= y) es ");
+
+
+//  console.log(x == 3 || x === y) && !(y !== 8 && x <= y);
+
+
+
+
+
+ //.......................
+
+//  let str = "";
+// let msj = "jaja!";
+// let esGracioso = "false";
+// console.log ((str || msj) && esGracioso);
+
+
+
+
+
+////.............
+
+// let edad =prompt("cual es su edad");
+// alert(edad>=18? "usted es mayor de edad":"usted es menor de edad");
+///::::::::::::::::::::::
