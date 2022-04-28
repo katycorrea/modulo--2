58feddc5059cb20ea402e03577877702f0d57b9e
katy <magdalena518xdgoldita@gmail.com>
2022-04-28 15:46:33 -0300

    clase 3 modulo 2

--- ejercicios asincronicos/M02C02/js/trabajo2.js
+++ ejercicios asincronicos/M02C02/js/trabajo2.js
@@ -1,29 +1,33 @@
- var numero = prompt("elige un numero para saber si es par o impar")
- if (numero % 2 == 0){
-     alert("el numero es par")
- }else{ alert ("es impar")
+//  var numero = prompt("elige un numero para saber si es par o impar")
+//  if (numero % 2 == 0){
+//      alert("el numero es par")
+//  }else{ alert ("es impar")
 
- }
-
-
-
-//-.........trabajo de edades y sexo
+//  }
 
 
-let sexo=prompt("Si es hombre ponga una M en cambio si es mujer una F");
+
+// //-.........trabajo de edades y sexo
+
+
+ let sexo=prompt("Si es hombre ponga una M en cambio si es mujer una F");
 let edad=prompt("escribe tu edad"); 
 
-if(sexo=="M" && edad>=65)
-{   alert("usted esta jubilado")
-}else if (sexo=="M" && edad<=64){
-    alert ("usted no esta jubilado")
-}
-else if (sexo=="F" && edad>=60){
-    alert("usted esta jubilada")
-}else if (sexo=="F" && edad<=59){
-    alert("usted no esta jubilada")
-}
+// if(sexo=="M" && edad>=65)
+// {   alert("usted esta jubilado")
+// }else if (sexo=="M" && edad<=64){
+//     alert ("usted no esta jubilado")
+// }
+// else if (sexo=="F" && edad>=60){
+//     alert("usted esta jubilada")
+// }else if (sexo=="F" && edad<=59){
+//     alert("usted no esta jubilada")
+// }
 
+
+ (sexo=="M" && edad>=65) || (sexo=="M" && edad<=64)? alert ("usted esta jubilado"): ("usted no esta jubilado")
+ 
+ (sexo=="F" && edad>=60) || (sexo=="F" && edad<=59)? alert ("usted esta jubilada"): ("usted no esta jubilada")
 
 
 
